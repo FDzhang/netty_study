9d0e0545571546720e7b6c574469ba2e571f04e0
zhangxinqiang <1933581361@qq.com>
2020-10-10 16:13:03 +0800

    fourthExample 心跳

---
+++ src/main/java/com/fd/netty/fourthexample/MyServerInitializer.java
@@ -0,0 +1,23 @@
+package com.fd.netty.fourthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2020/10/10 9:51
+ */
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5,7,10, TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
