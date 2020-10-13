fcc002f3a3d3c614e990f0f47556fb96b1877c5f
zhangxinqiang <1933581361@qq.com>
2020-10-13 11:13:32 +0800

    ProtoBuf 多协议消息支持

--- src/main/java/com/fd/netty/sixthexample/TestClientHandler.java
+++ src/main/java/com/fd/netty/sixthexample/TestClientHandler.java
@@ -3,6 +3,8 @@ package com.fd.netty.sixthexample;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.util.Random;
+
 /**
  * @author ：zxq
  * @date ：Created in 2020/10/10 13:38
@@ -17,12 +19,39 @@ public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.Pe
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        MyDataInfo.Person person = MyDataInfo.Person.newBuilder()
-                .setName("悟空")
-                .setAge(500)
-                .setAddress("花果山")
-                .build();
+        int randomInt = new Random().nextInt(3);
+        MyDataInfo.MyMessage myMessage = null;
+        if (0 == randomInt) {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.PersonType)
+                    .setPerson(
+                            MyDataInfo.Person.newBuilder()
+                                    .setName("悟空")
+                                    .setAge(500)
+                                    .setAddress("花果山")
+                                    .build()
+                    ).build();
 
-        ctx.channel().writeAndFlush(person);
+        } else if (1 == randomInt) {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.DogType)
+                    .setDog(
+                            MyDataInfo.Dog.newBuilder()
+                                    .setName("二哈")
+                                    .setAge("12")
+                                    .build()
+                    ).build();
+        } else {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.CatType)
+                    .setCat(
+                            MyDataInfo.Cat.newBuilder()
+                                    .setName("咖啡猫")
+                                    .setCity("冰")
+                                    .build()
+                    ).build();
+        }
+
+        ctx.channel().writeAndFlush(myMessage);
     }
 }
--- src/main/java/com/fd/netty/sixthexample/TestServerHandler.java
+++ src/main/java/com/fd/netty/sixthexample/TestServerHandler.java
@@ -8,11 +8,30 @@ import io.netty.channel.SimpleChannelInboundHandler;
  * @date ：Created in 2020/10/10 9:56
  */
 
-public class TestServerHandler extends SimpleChannelInboundHandler<MyDataInfo.Person> {
+public class TestServerHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.Person msg) throws Exception {
-        System.out.println(msg.getName());
-        System.out.println(msg.getAge());
-        System.out.println(msg.getAddress());
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
+        MyDataInfo.MyMessage.DataType dataType = msg.getDataType();
+        if (MyDataInfo.MyMessage.DataType.PersonType == dataType) {
+            MyDataInfo.Person person = msg.getPerson();
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+            System.out.println(person.getAddress());
+        } else if (MyDataInfo.MyMessage.DataType.DogType == dataType){
+            MyDataInfo.Dog dog = msg.getDog();
+            System.out.println(dog.getName());
+            System.out.println(dog.getAge());
+        } else {
+            MyDataInfo.Cat cat = msg.getCat();
+
+            System.out.println(cat.getName());
+            System.out.println(cat.getCity());
+        }
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
     }
 }
